44a7722d564039c97db19fd2e7a61c85a5783e05
NaughtyMonk <bogdanfrolov04@gmail.com>
2025-05-25 18:15:48 +0200

    BK slider update

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,135 +261,135 @@ const thumbNext = document.querySelector('.thumbnail-carousel-wrapper .arrow-btn
   showImage(0);
 });
 
-var curpage = 1;
-var sliding = false;
-var click = true;
-var left = document.getElementById("left");
-var right = document.getElementById("right");
-var pagePrefix = "slide";
-var pageShift = 500;
-var transitionPrefix = "circle";
-var svg = true;
+// var curpage = 1;
+// var sliding = false;
+// var click = true;
+// var left = document.getElementById("left");
+// var right = document.getElementById("right");
+// var pagePrefix = "slide";
+// var pageShift = 500;
+// var transitionPrefix = "circle";
+// var svg = true;
 
-function leftSlide() {
-	if (click) {
-		if (curpage == 1) curpage = 5;
-		console.log("woek");
-		sliding = true;
-		curpage--;
-		svg = true;
-		click = false;
-		for (k = 1; k <= 4; k++) {
-			var a1 = document.getElementById(pagePrefix + k);
-			a1.className += " tran";
-		}
-		setTimeout(() => {
-			move();
-		}, 200);
-		setTimeout(() => {
-			for (k = 1; k <= 4; k++) {
-				var a1 = document.getElementById(pagePrefix + k);
-				a1.classList.remove("tran");
-			}
-		}, 1400);
-	}
-}
+// function leftSlide() {
+// 	if (click) {
+// 		if (curpage == 1) curpage = 5;
+// 		console.log("woek");
+// 		sliding = true;
+// 		curpage--;
+// 		svg = true;
+// 		click = false;
+// 		for (k = 1; k <= 4; k++) {
+// 			var a1 = document.getElementById(pagePrefix + k);
+// 			a1.className += " tran";
+// 		}
+// 		setTimeout(() => {
+// 			move();
+// 		}, 200);
+// 		setTimeout(() => {
+// 			for (k = 1; k <= 4; k++) {
+// 				var a1 = document.getElementById(pagePrefix + k);
+// 				a1.classList.remove("tran");
+// 			}
+// 		}, 1400);
+// 	}
+// }
 
-function rightSlide() {
-	if (click) {
-		if (curpage == 4) curpage = 0;
-		console.log("woek");
-		sliding = true;
-		curpage++;
-		svg = false;
-		click = false;
-		for (k = 1; k <= 4; k++) {
-			var a1 = document.getElementById(pagePrefix + k);
-			a1.className += " tran";
-		}
-		setTimeout(() => {
-			move();
-		}, 200);
-		setTimeout(() => {
-			for (k = 1; k <= 4; k++) {
-				var a1 = document.getElementById(pagePrefix + k);
-				a1.classList.remove("tran");
-			}
-		}, 1400);
-	}
-}
+// function rightSlide() {
+// 	if (click) {
+// 		if (curpage == 4) curpage = 0;
+// 		console.log("woek");
+// 		sliding = true;
+// 		curpage++;
+// 		svg = false;
+// 		click = false;
+// 		for (k = 1; k <= 4; k++) {
+// 			var a1 = document.getElementById(pagePrefix + k);
+// 			a1.className += " tran";
+// 		}
+// 		setTimeout(() => {
+// 			move();
+// 		}, 200);
+// 		setTimeout(() => {
+// 			for (k = 1; k <= 4; k++) {
+// 				var a1 = document.getElementById(pagePrefix + k);
+// 				a1.classList.remove("tran");
+// 			}
+// 		}, 1400);
+// 	}
+// }
 
-function move() {
-	if (sliding) {
-		sliding = false;
-		if (svg) {
-			for (j = 1; j <= 9; j++) {
-				var c = document.getElementById(transitionPrefix + j);
-				c.classList.remove("steap");
-				c.setAttribute("class", transitionPrefix + j + " streak");
-				console.log("streak");
-			}
-		} else {
-			for (j = 10; j <= 18; j++) {
-				var c = document.getElementById(transitionPrefix + j);
-				c.classList.remove("steap");
-				c.setAttribute("class", transitionPrefix + j + " streak");
-				console.log("streak");
-			}
-		}
-		setTimeout(() => {
-			for (i = 1; i <= 4; i++) {
-				if (i == curpage) {
-					var a = document.getElementById(pagePrefix + i);
-					a.className += " up1";
-				} else {
-					var b = document.getElementById(pagePrefix + i);
-					b.classList.remove("up1");
-				}
-			}
-			sliding = true;
-		}, 600);
-		setTimeout(() => {
-			click = true;
-		}, 1700);
+// function move() {
+// 	if (sliding) {
+// 		sliding = false;
+// 		if (svg) {
+// 			for (j = 1; j <= 9; j++) {
+// 				var c = document.getElementById(transitionPrefix + j);
+// 				c.classList.remove("steap");
+// 				c.setAttribute("class", transitionPrefix + j + " streak");
+// 				console.log("streak");
+// 			}
+// 		} else {
+// 			for (j = 10; j <= 18; j++) {
+// 				var c = document.getElementById(transitionPrefix + j);
+// 				c.classList.remove("steap");
+// 				c.setAttribute("class", transitionPrefix + j + " streak");
+// 				console.log("streak");
+// 			}
+// 		}
+// 		setTimeout(() => {
+// 			for (i = 1; i <= 4; i++) {
+// 				if (i == curpage) {
+// 					var a = document.getElementById(pagePrefix + i);
+// 					a.className += " up1";
+// 				} else {
+// 					var b = document.getElementById(pagePrefix + i);
+// 					b.classList.remove("up1");
+// 				}
+// 			}
+// 			sliding = true;
+// 		}, 600);
+// 		setTimeout(() => {
+// 			click = true;
+// 		}, 1700);
 
-		setTimeout(() => {
-			if (svg) {
-				for (j = 1; j <= 9; j++) {
-					var c = document.getElementById(transitionPrefix + j);
-					c.classList.remove("streak");
-					c.setAttribute("class", transitionPrefix + j + " steap");
-				}
-			} else {
-				for (j = 10; j <= 18; j++) {
-					var c = document.getElementById(transitionPrefix + j);
-					c.classList.remove("streak");
-					c.setAttribute("class", transitionPrefix + j + " steap");
-				}
-				sliding = true;
-			}
-		}, 850);
-		setTimeout(() => {
-			click = true;
-		}, 1700);
-	}
-}
+// 		setTimeout(() => {
+// 			if (svg) {
+// 				for (j = 1; j <= 9; j++) {
+// 					var c = document.getElementById(transitionPrefix + j);
+// 					c.classList.remove("streak");
+// 					c.setAttribute("class", transitionPrefix + j + " steap");
+// 				}
+// 			} else {
+// 				for (j = 10; j <= 18; j++) {
+// 					var c = document.getElementById(transitionPrefix + j);
+// 					c.classList.remove("streak");
+// 					c.setAttribute("class", transitionPrefix + j + " steap");
+// 				}
+// 				sliding = true;
+// 			}
+// 		}, 850);
+// 		setTimeout(() => {
+// 			click = true;
+// 		}, 1700);
+// 	}
+// }
 
-left.onmousedown = () => {
-	leftSlide();
-};
+// left.onmousedown = () => {
+// 	leftSlide();
+// };
 
-right.onmousedown = () => {
-	rightSlide();
-};
+// right.onmousedown = () => {
+// 	rightSlide();
+// };
 
-document.onkeydown = e => {
-	if (e.keyCode == 37) {
-		leftSlide();
-	} else if (e.keyCode == 39) {
-		rightSlide();
-	}
-};
+// document.onkeydown = e => {
+// 	if (e.keyCode == 37) {
+// 		leftSlide();
+// 	} else if (e.keyCode == 39) {
+// 		rightSlide();
+// 	}
+// };
 
 //for codepen header
 // setTimeout(() => {
@@ -397,94 +397,94 @@ document.onkeydown = e => {
 // }, 500);
 
 // ============ Слайдер =============
-var curpage = 1;
-var sliding = false;
-var click = true;
-var left = document.getElementById("left");
-var right = document.getElementById("right");
-var pagePrefix = "slide";
-var pageShift = 500;
-var transitionPrefix = "circle";
-var svg = true;
+// var curpage = 1;
+// var sliding = false;
+// var click = true;
+// var left = document.getElementById("left");
+// var right = document.getElementById("right");
+// var pagePrefix = "slide";
+// var pageShift = 500;
+// var transitionPrefix = "circle";
+// var svg = true;
 
-function leftSlide() {
-	if (click) {
-		if (curpage == 1) curpage = 5;
-		sliding = true;
-		curpage--;
-		svg = true;
-		click = false;
-		for (let k = 1; k <= 4; k++) {
-			document.getElementById(pagePrefix + k).classList.add("tran");
-		}
-		setTimeout(move, 200);
-		setTimeout(() => {
-			for (let k = 1; k <= 4; k++) {
-				document.getElementById(pagePrefix + k).classList.remove("tran");
-			}
-		}, 1400);
-	}
-}
+// function leftSlide() {
+// 	if (click) {
+// 		if (curpage == 1) curpage = 5;
+// 		sliding = true;
+// 		curpage--;
+// 		svg = true;
+// 		click = false;
+// 		for (let k = 1; k <= 4; k++) {
+// 			document.getElementById(pagePrefix + k).classList.add("tran");
+// 		}
+// 		setTimeout(move, 200);
+// 		setTimeout(() => {
+// 			for (let k = 1; k <= 4; k++) {
+// 				document.getElementById(pagePrefix + k).classList.remove("tran");
+// 			}
+// 		}, 1400);
+// 	}
+// }
 
-function rightSlide() {
-	if (click) {
-		if (curpage == 4) curpage = 0;
-		sliding = true;
-		curpage++;
-		svg = false;
-		click = false;
-		for (let k = 1; k <= 4; k++) {
-			document.getElementById(pagePrefix + k).classList.add("tran");
-		}
-		setTimeout(move, 200);
-		setTimeout(() => {
-			for (let k = 1; k <= 4; k++) {
-				document.getElementById(pagePrefix + k).classList.remove("tran");
-			}
-		}, 1400);
-	}
-}
+// function rightSlide() {
+// 	if (click) {
+// 		if (curpage == 4) curpage = 0;
+// 		sliding = true;
+// 		curpage++;
+// 		svg = false;
+// 		click = false;
+// 		for (let k = 1; k <= 4; k++) {
+// 			document.getElementById(pagePrefix + k).classList.add("tran");
+// 		}
+// 		setTimeout(move, 200);
+// 		setTimeout(() => {
+// 			for (let k = 1; k <= 4; k++) {
+// 				document.getElementById(pagePrefix + k).classList.remove("tran");
+// 			}
+// 		}, 1400);
+// 	}
+// }
 
-function move() {
-	if (sliding) {
-		sliding = false;
-		const range = svg ? [1, 9] : [10, 18];
-		for (let j = range[0]; j <= range[1]; j++) {
-			let c = document.getElementById(transitionPrefix + j);
-			c.classList.remove("steap");
-			c.setAttribute("class", transitionPrefix + j + " streak");
-		}
-		setTimeout(() => {
-			for (let i = 1; i <= 4; i++) {
-				let slide = document.getElementById(pagePrefix + i);
-				if (i == curpage) {
-					slide.classList.add("up1");
-				} else {
-					slide.classList.remove("up1");
-				}
-			}
-			sliding = true;
-		}, 600);
-		setTimeout(() => {
-			click = true;
-		}, 1700);
-		setTimeout(() => {
-			for (let j = range[0]; j <= range[1]; j++) {
-				let c = document.getElementById(transitionPrefix + j);
-				c.classList.remove("streak");
-				c.setAttribute("class", transitionPrefix + j + " steap");
-			}
-		}, 850);
-	}
-}
+// function move() {
+// 	if (sliding) {
+// 		sliding = false;
+// 		const range = svg ? [1, 9] : [10, 18];
+// 		for (let j = range[0]; j <= range[1]; j++) {
+// 			let c = document.getElementById(transitionPrefix + j);
+// 			c.classList.remove("steap");
+// 			c.setAttribute("class", transitionPrefix + j + " streak");
+// 		}
+// 		setTimeout(() => {
+// 			for (let i = 1; i <= 4; i++) {
+// 				let slide = document.getElementById(pagePrefix + i);
+// 				if (i == curpage) {
+// 					slide.classList.add("up1");
+// 				} else {
+// 					slide.classList.remove("up1");
+// 				}
+// 			}
+// 			sliding = true;
+// 		}, 600);
+// 		setTimeout(() => {
+// 			click = true;
+// 		}, 1700);
+// 		setTimeout(() => {
+// 			for (let j = range[0]; j <= range[1]; j++) {
+// 				let c = document.getElementById(transitionPrefix + j);
+// 				c.classList.remove("streak");
+// 				c.setAttribute("class", transitionPrefix + j + " steap");
+// 			}
+// 		}, 850);
+// 	}
+// }
 
-left.onmousedown = leftSlide;
-right.onmousedown = rightSlide;
+// left.onmousedown = leftSlide;
+// right.onmousedown = rightSlide;
 
-document.onkeydown = e => {
-	if (e.keyCode == 37) leftSlide();
-	else if (e.keyCode == 39) rightSlide();
-};
+// document.onkeydown = e => {
+// 	if (e.keyCode == 37) leftSlide();
+// 	else if (e.keyCode == 39) rightSlide();
+// };
 
 // ============ Автопереключение с паузой =============
 let autoSlideInterval;
@@ -634,8 +634,276 @@ function openFullscreen(index) {
 	document.body.appendChild(overlay);
 }
 
+// slideElements.forEach((slide, idx) => {
+// 	slide.addEventListener('click', () => {
+// 		openFullscreen(idx);
+// 	});
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+	let curpage = 1;
+	let sliding = false;
+	let click = true;
+	let pagePrefix = "slide";
+	let transitionPrefix = "circle";
+	let svg = true;
+
+	const left = document.getElementById("left");
+	const right = document.getElementById("right");
+
+	// Получаем общее количество слайдов динамически
+	const totalSlides = document.querySelectorAll(`[id^="${pagePrefix}"]`).length;
+
+	function leftSlide() {
+		if (click) {
+			if (curpage == 1) curpage = totalSlides + 1;
+			sliding = true;
+			curpage--;
+			svg = true;
+			click = false;
+
+			for (let k = 1; k <= totalSlides; k++) {
+				const a1 = document.getElementById(pagePrefix + k);
+				if (a1) a1.classList.add("tran");
+			}
+
+			setTimeout(() => move(), 200);
+
+			setTimeout(() => {
+				for (let k = 1; k <= totalSlides; k++) {
+					const a1 = document.getElementById(pagePrefix + k);
+					if (a1) a1.classList.remove("tran");
+				}
+			}, 1400);
+		}
+	}
+
+	function rightSlide() {
+		if (click) {
+			if (curpage == totalSlides) curpage = 0;
+			sliding = true;
+			curpage++;
+			svg = false;
+			click = false;
+
+			for (let k = 1; k <= totalSlides; k++) {
+				const a1 = document.getElementById(pagePrefix + k);
+				if (a1) a1.classList.add("tran");
+			}
+
+			setTimeout(() => move(), 200);
+
+			setTimeout(() => {
+				for (let k = 1; k <= totalSlides; k++) {
+					const a1 = document.getElementById(pagePrefix + k);
+					if (a1) a1.classList.remove("tran");
+				}
+			}, 1400);
+		}
+	}
+
+	function move() {
+		if (sliding) {
+			sliding = false;
+
+			const start = svg ? 1 : 10;
+			const end = svg ? 9 : 18;
+			for (let j = start; j <= end; j++) {
+				const c = document.getElementById(transitionPrefix + j);
+				if (c) {
+					c.classList.remove("steap");
+					c.setAttribute("class", `${transitionPrefix}${j} streak`);
+				}
+			}
+
+			setTimeout(() => {
+				for (let i = 1; i <= totalSlides; i++) {
+					const slide = document.getElementById(pagePrefix + i);
+					if (!slide) continue;
+					if (i === curpage) {
+						slide.classList.add("up1");
+					} else {
+						slide.classList.remove("up1");
+					}
+				}
+				sliding = true;
+			}, 600);
+
+			setTimeout(() => {
+				for (let j = start; j <= end; j++) {
+					const c = document.getElementById(transitionPrefix + j);
+					if (c) {
+						c.classList.remove("streak");
+						c.setAttribute("class", `${transitionPrefix}${j} steap`);
+					}
+				}
+			}, 850);
+
+			setTimeout(() => {
+				click = true;
+			}, 1700);
+		}
+	}
+
+	left?.addEventListener("mousedown", leftSlide);
+	right?.addEventListener("mousedown", rightSlide);
+// === АВТОПЕРЕКЛЮЧЕНИЕ СЛАЙДЕРА ===
+const carousel = document.querySelector('.parent');
+let autoSlideInterval;
+let isHovered = false;
+const progressBar = document.getElementById('autoProgressBar');
+
+function startAutoSlide() {
+	if (!progressBar) return;
+
+	progressBar.style.transition = 'none';
+	progressBar.style.width = '0%';
+	setTimeout(() => {
+		progressBar.style.transition = 'width 5s linear';
+		progressBar.style.width = '100%';
+	}, 50);
+
+	autoSlideInterval = setInterval(() => {
+		if (click && !isHovered) {
+			rightSlide();
+			resetAutoProgressBar();
+		}
+	}, 5000);
+}
+
+function resetAutoProgressBar() {
+	if (!progressBar) return;
+	progressBar.style.transition = 'none';
+	progressBar.style.width = '0%';
+	setTimeout(() => {
+		progressBar.style.transition = 'width 5s linear';
+		progressBar.style.width = '100%';
+	}, 50);
+}
+
+function stopAutoSlide() {
+	clearInterval(autoSlideInterval);
+	if (progressBar) progressBar.style.width = '0%';
+}
+
+if (carousel) {
+	carousel.addEventListener('mouseenter', () => {
+		isHovered = true;
+		stopAutoSlide();
+	});
+	carousel.addEventListener('mouseleave', () => {
+		isHovered = false;
+		startAutoSlide();
+	});
+	startAutoSlide();
+}
+
+// === ПОЛНОЭКРАННЫЙ РЕЖИМ ===
+const slideElements = Array.from(document.querySelectorAll(`[id^='slide']`));
+const imageUrls = slideElements.map(slide => {
+	const bg = getComputedStyle(slide).backgroundImage;
+	const match = bg.match(/url\(["']?(.*?)["']?\)/);
+	return match ? match[1] : null;
+});
+
+let fullscreenIndex = 0;
+
+function openFullscreen(index) {
+	fullscreenIndex = index;
+
+	const overlay = document.createElement('div');
+	overlay.id = 'fullscreenOverlay';
+	Object.assign(overlay.style, {
+		position: 'fixed',
+		top: 0,
+		left: 0,
+		width: '100vw',
+		height: '100vh',
+		backgroundColor: 'rgba(0,0,0,0.95)',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		zIndex: 10000,
+		cursor: 'default'
+	});
+
+	const img = document.createElement('img');
+	img.src = imageUrls[fullscreenIndex];
+	Object.assign(img.style, {
+		maxWidth: '90vw',
+		maxHeight: '90vh',
+		objectFit: 'contain',
+		pointerEvents: 'none',
+		transition: 'opacity 0.3s ease'
+	});
+	overlay.appendChild(img);
+
+	// Левая зона
+	const left = document.createElement('div');
+	Object.assign(left.style, {
+		position: 'absolute',
+		left: 0,
+		top: 0,
+		width: '30%',
+		height: '100%',
+		cursor: 'pointer',
+		background: 'linear-gradient(to right, rgba(217, 255, 3, 0.1), transparent)',
+		zIndex: 10001
+	});
+	left.onmouseenter = () => left.style.background = 'linear-gradient(to right, rgba(255, 215, 0, 0.6), transparent)';
+	left.onmouseleave = () => left.style.background = 'linear-gradient(to right, rgba(217, 255, 3, 0.1), transparent)';
+	left.onclick = () => {
+		fullscreenIndex = (fullscreenIndex - 1 + imageUrls.length) % imageUrls.length;
+		img.src = imageUrls[fullscreenIndex];
+	};
+
+	// Правая зона
+	const right = document.createElement('div');
+	Object.assign(right.style, {
+		position: 'absolute',
+		right: 0,
+		top: 0,
+		width: '30%',
+		height: '100%',
+		cursor: 'pointer',
+		background: 'linear-gradient(to left, rgba(217, 255, 3, 0.1), transparent)',
+		zIndex: 10001
+	});
+	right.onmouseenter = () => right.style.background = 'linear-gradient(to left, rgba(255, 215, 0, 0.6), transparent)';
+	right.onmouseleave = () => right.style.background = 'linear-gradient(to left, rgba(217, 255, 3, 0.1), transparent)';
+	right.onclick = () => {
+		fullscreenIndex = (fullscreenIndex + 1) % imageUrls.length;
+		img.src = imageUrls[fullscreenIndex];
+	};
+
+	// Центр — закрытие
+	const center = document.createElement('div');
+	Object.assign(center.style, {
+		position: 'absolute',
+		left: '30%',
+		width: '40%',
+		top: 0,
+		height: '100%',
+		cursor: 'default',
+		zIndex: 10001
+	});
+	center.onclick = () => document.body.removeChild(overlay);
+
+	overlay.appendChild(left);
+	overlay.appendChild(right);
+	overlay.appendChild(center);
+	document.body.appendChild(overlay);
+}
+
+// Назначаем клик по каждому слайду
 slideElements.forEach((slide, idx) => {
 	slide.addEventListener('click', () => {
 		openFullscreen(idx);
+	});
+});
+
+	document.addEventListener("keydown", e => {
+		if (e.key === "ArrowLeft") leftSlide();
+		if (e.key === "ArrowRight") rightSlide();
 	});
 });
